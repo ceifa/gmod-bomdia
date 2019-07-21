44675670060dded3429d416120a1f628e43a2cee
ceifa <gabrinelson27@gmail.com>
2019-07-21 08:44:17 -0300

    Fix code consistence

--- lua/bomdia/cl_bomdia.lua
+++ lua/bomdia/cl_bomdia.lua
@@ -1,12 +1,11 @@
 if CLIENT then
     net.Receive("write_chat", function()
-        local parse = util.JSONToTable( net.ReadString() )
+        local parse = util.JSONToTable(net.ReadString())
         local cabuloso = net.ReadBit()
-        
-        chat.AddText( unpack( parse ) )
+        chat.AddText(unpack(parse))
 
         if cabuloso == 1 then
-            surface.PlaySound( "ambient/explosions/exp1.wav" )
+            surface.PlaySound("ambient/explosions/exp1.wav")
         end
     end)
 end
--- lua/bomdia/sv_bomdia.lua
+++ lua/bomdia/sv_bomdia.lua
@@ -1,67 +1,52 @@
 if SERVER then
-	local possibleCommands = {
-		"bom dia",
-		"good morning",
-		"buenos dias"
-	}
+    local possibleCommands = {"bom dia", "good morning", "buenos dias"}
+    sql.Query("CREATE TABLE IF NOT EXISTS bomdia ( `sid64` STRING, `bomdias` INT, PRIMARY KEY(sid64) )")
 
-	sql.Query("CREATE TABLE IF NOT EXISTS bomdia ( `sid64` STRING, `bomdias` INT, PRIMARY KEY(sid64) )")
+    hook.Add("PlayerSay", "HelloCommand", function(ply, strText, bTeam)
+        local strTextMin = string.lower(strText)
 
-	hook.Add( "PlayerSay", "HelloCommand", function( ply, strText, bTeam )
-		local strTextMin = string.lower( strText )
+        if table.HasValue(possibleCommands, strTextMin) and not ply.lastBomDia or ply.lastBomDia < CurTime() then
+            ply.lastBomDia = CurTime() + GetConVar("bomdia_interval_time"):GetInt()
+            sql.Query("INSERT OR IGNORE INTO `bomdia` (sid64, bomdias) VALUES ('" .. ply:SteamID64() .. "', 0); UPDATE `bomdia` SET bomdias = bomdias + 1 WHERE sid64 = '" .. ply:SteamID64() .. "'")
+            local bomdia = math.random()
 
-		if table.HasValue(possibleCommands, strTextMin) then
-			if not ply.lastBomDia or ply.lastBomDia < CurTime() then
+            if PS and bomdia > GetConVar("bomdia_cabuloso_rate"):GetFloat() then
+                GiveBomDiaCabuloso(ply, strTextMin)
+            else
+                GiveBomDia(ply, strTextMin)
+            end
 
-				ply.lastBomDia = CurTime() + GetConVar("bomdia_interval_time"):GetInt()
+            return ""
+        end
+    end)
 
-				sql.Query("INSERT OR IGNORE INTO `bomdia` (sid64, bomdias) VALUES ('"..ply:SteamID64().."', 0); UPDATE `bomdia` SET bomdias = bomdias + 1 WHERE sid64 = '"..ply:SteamID64().."'")
-				
-				local bomdia = math.random()
-				
-				if PS and bomdia > GetConVar("bomdia_cabuloso_rate"):GetFloat() then
-					GiveBomDiaCabuloso(ply, strTextMin)
-				else
-					GiveBomDia(ply, strTextMin)				
-				end
-				
-				return ""
-			end
-		end
+    util.AddNetworkString("write_chat")
 
-	end )
-	
-	util.AddNetworkString( "write_chat" )
+    function GiveBomDia(ply, command)
+        local color = Color(math.random(0, 255), math.random(0, 255), math.random(0, 255))
+        local parse = {}
+        table.insert(parse, color)
+        table.insert(parse, ply:GetName() .. " deu " .. command .. " para todos do servidor!")
+        net.Start("write_chat")
+        net.WriteString(util.TableToJSON(parse))
+        net.WriteBit(0)
+        net.Broadcast()
+    end
 
-	function GiveBomDia(ply, command)
-		local color = Color( math.random(0, 255), math.random(0, 255), math.random(0, 255) )
-
-		local parse = {}
-		table.insert( parse, color )
-		table.insert( parse, ply:GetName().." deu "..command.." para todos do servidor!" )
-
-		net.Start("write_chat")
-			net.WriteString(util.TableToJSON(parse))
-			net.WriteBit(0)
-		net.Broadcast()
-	end
-
-	function GiveBomDiaCabuloso(ply, command)
+    function GiveBomDiaCabuloso(ply, command)
         local points = GetConVar("bomdia_cabuloso_points"):GetInt()
         local pointsName = PS.Config and PS.Config.PointsName or "pontos"
-        local color = Color( 255, 215, 0 )
+        local color = Color(255, 215, 0)
+        local parse = {}
+        table.insert(parse, color)
+        table.insert(parse, ply:GetName() .. " deu um " .. command .. " cabuloso! Todos os jogadores ganharam " .. points .. " " .. pointsName .. "!")
+        net.Start("write_chat")
+        net.WriteString(util.TableToJSON(parse))
+        net.WriteBit(1)
+        net.Broadcast()
 
-		local parse = {}
-		table.insert( parse, color )
-		table.insert( parse, ply:GetName().." deu um "..command.." cabuloso! Todos os jogadores ganharam "..points.." "..pointsName.."!" )
-
-		net.Start("write_chat")
-			net.WriteString(util.TableToJSON(parse))
-			net.WriteBit(1)
-		net.Broadcast()
-		
-		for k, v in pairs( player.GetAll() ) do
-			v:PS_GivePoints(points)
-		end
-	end
+        for _, v in pairs(player.GetAll()) do
+            v:PS_GivePoints(points)
+        end
+    end
 end
